77959502c3cb207299438e081217d30059254cc1
Jhhoo <jonathan.ho@uken.com>
2017-02-12 16:20:10 -0500

    Refactor thread settings v2

--- app.js
+++ app.js
@@ -923,7 +923,7 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
   //setGreetingText();
   //setGetStarted();
-  createGreetingApitest();
+  //createGreetingApitest();
 });
 
 module.exports = app;
