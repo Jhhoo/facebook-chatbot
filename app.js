32968735d1a1432091078691e123ecac7ee1dc1e
Jhhoo <jonathan.ho@uken.com>
2017-02-12 14:50:46 -0500

    Image test fix v2

--- app.js
+++ app.js
@@ -435,7 +435,7 @@ function sendImageMessage(recipientId) {
       attachment: {
         type: "image",
         payload: {
-          url: "https://aqueous-beach-48898.herokuapp.com/webhook/rift.png"
+          url: "https://aqueous-beach-48898.herokuapp.com/rift.png"
         }
       }
     }
